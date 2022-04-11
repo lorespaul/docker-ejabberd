38cbc58be0ae9e4de1f5ebdfeb7815f237bfb727
Lorenzo Daneo <lorenzo.daneo91@gmail.com>
2022-04-11 18:12:14 +0200

    build in apple silicon with ./build.sh 21.12 --arm

--- ecs/build.sh
+++ ecs/build.sh
@@ -3,10 +3,29 @@
 current=$(date +%y.%m)
 version=${1:-$current}
 
-docker build \
-    --build-arg VERSION=$version \
-    --build-arg VCS_REF=`git rev-parse --short HEAD` \
-    --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-    -t kitwts_ejabberd/ecs:$version .
+arguments=( "$@" )
+
+for arg in ${!arguments[@]}
+do
+    if [ "${arguments[$arg]}" == "--arm" ]; then
+        PLATFORM="ARM"
+    fi
+done
+
+if [ ! -z "$PLATFORM" ];
+then
+    docker build \
+        --build-arg VERSION=$version \
+        --build-arg VCS_REF=`git rev-parse --short HEAD` \
+        --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+        --platform linux/amd64 \
+        -t kitwts_ejabberd/ecs:$version .
+else
+    docker build \
+        --build-arg VERSION=$version \
+        --build-arg VCS_REF=`git rev-parse --short HEAD` \
+        --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
+        -t kitwts_ejabberd/ecs:$version .
+fi
 
 [ "$version" = "latest" ] || docker tag kitwts_ejabberd/ecs:$version kitwts_ejabberd/ecs:latest
