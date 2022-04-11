a4f2e23e6438c34a115f105488c7558b87d6b92d
Lorenzo Daneo <lorenzo.daneo91@gmail.com>
2022-04-11 11:48:21 +0200

    my image

--- ecs/build.sh
+++ ecs/build.sh
@@ -7,6 +7,6 @@ docker build \
     --build-arg VERSION=$version \
     --build-arg VCS_REF=`git rev-parse --short HEAD` \
     --build-arg BUILD_DATE=`date -u +"%Y-%m-%dT%H:%M:%SZ"` \
-    -t ejabberd/ecs:$version .
+    -t kitwts_ejabberd/ecs:$version .
 
-[ "$version" = "latest" ] || docker tag ejabberd/ecs:$version ejabberd/ecs:latest
+[ "$version" = "latest" ] || docker tag kitwts_ejabberd/ecs:$version kitwts_ejabberd/ecs:latest
